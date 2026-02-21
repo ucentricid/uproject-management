f969d824635d204a1bce4c088ca8638f0951f9dd
dedirosandiaj <dedirosandi.artajasa@gmail.com>
2026-02-21 22:59:54 +0700

    Update

--- lib/db.ts
+++ lib/db.ts
@@ -2,11 +2,7 @@ import { Pool } from 'pg'
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
 
-const connectionString = process.env.DATABASE_URL;
-
-if (!connectionString) {
-    throw new Error("DATABASE_URL must be defined");
-}
+const connectionString = process.env.DATABASE_URL || "postgresql://dummy:dummy@localhost:5432/dummy";
 
 const pool = new Pool({ connectionString })
 const adapter = new PrismaPg(pool)
